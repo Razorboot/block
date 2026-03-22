b9c33f9944e34a27dfde84d2f4cf67f321e47205
rjindael <me@rjindael.xyz>
2026-03-22 02:38:45 -0700

    feat: more WIP ui and use godot4.7 with patch

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,7 +1,27 @@
 extends Control
 
 func _ready() -> void:
-	$NewGameBtn.pressed.connect(_on_new_game_pressed)
+	$Menu/SingleplayerBtn.pressed.connect(_on_singleplayer)
+	$Menu/MultiplayerBtn.pressed.connect(_on_multiplayer)
+	$Menu/CharacterBtn.pressed.connect(_on_character)
+	$Menu/ModsBtn.pressed.connect(_on_mods)
+	$Menu/DetailsContainer/OptionsBtn.pressed.connect(_on_options)
+	$Menu/DetailsContainer/QuitBtn.pressed.connect(_on_quit)
 
-func _on_new_game_pressed() -> void:
+func _on_singleplayer() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_multiplayer() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_character() -> void:
+	$CustomizeCharacter.visible = true
+
+func _on_mods() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_options() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_quit() -> void:
+	get_tree().quit()
